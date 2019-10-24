b26bb545371b4fea478b2ac199d143b433c4fd9f
demuro1 <demuro1@mac.com>
2019-10-24 10:11:55 -0700

    This commit is adding the first 2 basic lessons

---
+++ java_cert_course/lesson_3.5/java_basics/src/Variables/src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main (String args[]){
+//        this course was a basic example on variables.  They talked about int and String.
+        int x = 234;
+        String words = "This is a sentence and is kind of a bullshit lesson.  IS this rally going to get me Java certified?";
+        System.out.println(x);
+        System.out.println(x + 4);
+        System.out.println(words);
+        System.out.println(words + " God please tell me this gets better?");
+
+
+    }
+}
