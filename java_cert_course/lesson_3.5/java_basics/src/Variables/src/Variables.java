9cae48ee08a88a84d01b208953a2b4019f58da3f
demuro1 <demuro1@mac.com>
2019-10-24 10:20:23 -0700

    I messed up the branches and commited this lesson in the lesson_3.5 stuff

--- java_cert_course/lesson_3.5/java_basics/src/Variables/src/Variables.java
+++ java_cert_course/lesson_3.5/java_basics/src/Variables/src/Variables.java
@@ -1,6 +1,8 @@
 public class Variables {
     public static void main (String args[]){
 //        this course was a basic example on variables.  They talked about int and String.
+//        I messed up the branches and commited this lesson in the lesson_3.5 stuff
+
         int x = 234;
         String words = "This is a sentence and is kind of a bullshit lesson.  IS this rally going to get me Java certified?";
         System.out.println(x);
