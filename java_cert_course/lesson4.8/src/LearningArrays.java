c3e95ff3c981b002f25f2210f625006ee738a093
demuro1 <demuro1@mac.com>
2019-10-24 10:59:59 -0700

    lesson4.8: this lesson dealt with an intro to arrays

--- java_cert_course/lesson4.8/src/LearningArrays.java
+++ java_cert_course/lesson4.8/src/LearningArrays.java
@@ -1,2 +1,8 @@
 public class LearningArrays {
+    public static void main(String args[]){
+        int []values = new int[100];
+        values[2] = 9;
+        values[99] = 10;
+        System.out.println(values[2]);
+    }
 }
