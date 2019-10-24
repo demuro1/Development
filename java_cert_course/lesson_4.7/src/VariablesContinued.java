af9ccf61005d217e1dfa0de37315f91b5ed90020
demuro1 <demuro1@mac.com>
2019-10-24 10:35:12 -0700

    lesson_4.7: this lesson continued the discussion on primitive data types

---
+++ java_cert_course/lesson_4.7/src/VariablesContinued.java
@@ -0,0 +1,14 @@
+public class VariablesContinued {
+    public static void main(String args[]){
+        // this is a continuation of the basics of variables.
+        int var = 1000000000;
+        long bigNumber = 100000000000000000L;
+        short smallNumber = 32767;
+        byte reallySmall = 127;
+        double decimalNumber = 394.003;
+        boolean decision = false;
+        char letter = '9';
+        
+        System.out.println(var);
+    }
+}
