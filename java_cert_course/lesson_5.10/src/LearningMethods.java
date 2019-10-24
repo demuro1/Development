91e29d7f94ded0c9409697ae09994d785c56d034
demuro1 <demuro1@mac.com>
2019-10-24 12:14:45 -0700

    lesson_5.10: this lesson deatl with methods and calling methods form other classes

---
+++ java_cert_course/lesson_5.10/src/LearningMethods.java
@@ -0,0 +1,13 @@
+
+
+public class LearningMethods {
+    public static void main(String args[]){
+//        This lesson deals with defining methods
+        System.out.println("This lesson deals with defining methods");
+        MyUtils.printSomeJunk("this is lame!!!");
+    }
+
+
+    
+
+    }
---
+++ java_cert_course/lesson_5.10/src/MyUtils.java
@@ -0,0 +1,5 @@
+public class MyUtils {
+    public static void printSomeJunk(String argument){
+        System.out.println("Meh " + argument);
+    }
+}
